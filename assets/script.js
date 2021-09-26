8d79c30b23f27982187401b4de1ad1c060446e50
Vince <starryblue7@gmail.com>
2021-09-26 12:18:37 -0700

    create quiz questions object data structure

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// fetch("./questions.json");
+
+// console.log(questions);
+
+// function createQuestion() {
+//     return JSON.parse(questions.json);
+// }
+
+
+quiz.forEach(quizItem => {
+    quizItem.category;
+    quizItem.questions.forEach(question => {
+        question.question;
+        question.answers.forEach(answer => {
+            
+        })
+    })
+});
+
+console.log(quiz[0].questions[0].question);
+console.log(quiz[0].questions[0].answers[0]);
+console.log(number);
