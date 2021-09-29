c970b443f8f68f90e45e250f1b8ad05545e6a399
Vince <starryblue7@gmail.com>
2021-09-28 20:08:38 -0700

    add complete question set

--- assets/quiz-questions.js
+++ assets/quiz-questions.js
@@ -3,20 +3,52 @@ const quiz = [
         "category": "JavaScript",
         "questions": [
             {
-                "question": "JavaScript question 1",
-                "answers": ["JS Answer 11", "JS Answer 12", "JS Answer 13", "JS Answer 14"]
+                "question": "What is the command for printing to console?",
+                "answers": ["console.log()", "output.log()", "toConsole()", "console.out()"]
             },
             {
-                "question": "JavaScript question 2",
-                "answers": ["JS Answer 21", "JS Answer 22", "JS Answer 23", "JS Answer 24"]
+                "question": "Which of these does not declare a variable?",
+                "answers": ["num", "var", "let", "const"]
             },
             {
-                "question": "JavaScript question 3",
-                "answers": ["JS Answer 31", "JS Answer 32", "JS Answer 33", "JS Answer 34"]
+                "question": "Which of these is not a primitive data type?",
+                "answers": ["Value", "String", "Symbol", "Null"]
             },
             {
-                "question": "Are squirrels the best?",
-                "answers": ["Obviously", "Squirrel?", "I guess", "No"]
+                "question": "How many possible values can booleans have?",
+                "answers": ["Two", "One", "Three", "Unlimited"]
+            },
+            {
+                "question": "Which conditional requires equal value but not type?",
+                "answers": ["x == y", "x === y", "x !== y", "x = y"]
+            },
+            {
+                "question": "Which of these in a conditional statement signify 'or'?",
+                "answers": ["||", "&&", "or", "><"]
+            },
+            {
+                "question": "Which of these signifies 'if x is not true'?",
+                "answers": ["if (!x)", "else if (x)", "if (x !== false)", "if !(x = true)"]
+            },
+            {
+                "question": "At which index is x in the array [a, b, x, y]?",
+                "answers": ["2", "1", "3", "4"]
+            },
+            {
+                "question": "How many cycles are in this loop? for(var i = 0; i < 3; i++)",
+                "answers": ["Three", "Four", "Two", "Infinite"]
+            },
+            {
+                "question": "What will array.pop() remove from an array?",
+                "answers": ["Last element", "First element", "All elements", "Nothing"]
+            },
+            {
+                "question": "How many properties can an object have?",
+                "answers": ["Unlimited", "Zero", "Up to ten", "One"]
+            },
+            {
+                "question": "What must be given to satisfy a function's parameters?",
+                "answers": ["Arguments", "Local variables", "Conditions", "Indices"]
             }
         ]
     }
@@ -25,12 +57,52 @@ const quiz = [
         "category": "Marvel",
         "questions": [
             {
-                "question": "Marvel question 1",
-                "answers": ["M Answer 1", "M Answer 2", "M Answer 3", "M Answer 4"]
+                "question": "In the Days of Future Past comic book run, who is sent to the past?",
+                "answers": ["Kitty Pryde", "Wolverine", "Professor X", "Bishop"]
             },
             {
-                "question": "Are giraffes the best?",
-                "answers": ["Nah", "Squirrel?", "I guess", "YES"]
+                "question": "What is Ororo Munroe's mutant power?",
+                "answers": ["Weather", "Teleportation", "Telepathy", "Voodoo"]
+            },
+            {
+                "question": "Which newspaper does J. Jonah Jameson work for?",
+                "answers": ["The Daily Bugle", "The Daily Planet", "The New Yorker", "The New York Herald"]
+            },
+            {
+                "question": "What planet is Drax the Destroyer from?",
+                "answers": ["Earth", "Titan", "Xandar", "Saturn"]
+            },
+            {
+                "question": "Namor is the ruler of which realm?",
+                "answers": ["Atlantis", "Wakanda", "Krakoa", "Asgard"]
+            },
+            {
+                "question": "Which power source does Jean Grey have access to?",
+                "answers": ["The Phoenix Force", "The Power Cosmic", "Chaos Magic", "Negative Energy"]
+            },
+            {
+                "question": "In which Realm was Loki born?",
+                "answers": ["Jotunheim", "Asgard", "Nidavellir", "Alfheim"]
+            },
+            {
+                "question": "How did the Fantastic Four get their powers?",
+                "answers": ["Cosmic rays", "Gamma ray burst", "Mutation", "A curse"]
+            },
+            {
+                "question": "Which of The Watchers primarily observes Earth-616?",
+                "answers": ["Uatu", "Ubuntu", "Umar", "Ultimo"]
+            },
+            {
+                "question": "Which of these characters is the sister of Captain Britain?",
+                "answers": ["Psylocke", "Agatha Harkness", "Wanda Maximoff", "Pepper Potts"]
+            },
+            {
+                "question": "Captain America was the product of which program?",
+                "answers": ["Project: Rebirth", "Weapon X", "Winter Soldier", "Avengers"]
+            },
+            {
+                "question": "Whose brainwaves were used to create The Vision?",
+                "answers": ["Wonder Man", "Iron Man", "Hulk", "Hawkweye"]
             }
         ]
     }
